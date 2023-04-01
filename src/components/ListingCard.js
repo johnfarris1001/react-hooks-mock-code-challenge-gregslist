8fbb07860ed77e8db97aec41623154499b768cf1
John <johnfarris217@gmail.com>
2023-03-31 21:06:28 -0500

    basic deliverables completed

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ListingCard() {
+function ListingCard({ listing, handleDelete }) {
+  const [favorite, setFavorite] = useState(true)
+
+  function toggleFavorite() {
+    setFavorite(!favorite)
+  }
+
+  function handleDeleteClick() {
+    fetch(`http://localhost:6001/listings/${listing.id}`, {
+      method: 'DELETE'
+    })
+      .then(resp => resp.json())
+      .then(() => handleDelete(listing))
+  }
+
   return (
     <li className="card">
       <div className="image">
         <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
+        <img src={listing.image} alt={listing.description} />
       </div>
       <div className="details">
-        {true ? (
-          <button className="emoji-button favorite active">★</button>
+        {favorite ? (
+          <button onClick={toggleFavorite} className="emoji-button favorite active">★</button>
         ) : (
-          <button className="emoji-button favorite">☆</button>
+          <button onClick={toggleFavorite} className="emoji-button favorite">☆</button>
         )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
-        <button className="emoji-button delete">🗑</button>
+        <strong>{listing.description}</strong>
+        <span> · {listing.location}</span>
+        <button onClick={handleDeleteClick} className="emoji-button delete">🗑</button>
       </div>
     </li>
   );
